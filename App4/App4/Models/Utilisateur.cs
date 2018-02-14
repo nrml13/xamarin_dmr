b858288b5c3bc906c6269299b284855407e35c1d
Nrml13 <gatto.marlon.7@gmail.com>
2018-02-14 16:25:03 +0100

    dernier commit formation xamarin

--- App4/App4/Models/Utilisateur.cs
+++ App4/App4/Models/Utilisateur.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Ceci est un POCO (ou s'en rapproche !), en gros cela veux dire que cette classe ne contient que des propriétés
+/// </summary>
+
+
 namespace App4.Models
 {
     public class Utilisateur
@@ -12,5 +17,11 @@ namespace App4.Models
         public String MotDePasse { get; set; }
         public DateTime DateDeNaissance { get; set; }
         public List<String> Maladies { get; set; }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return NomUtilisateur;
+        }
     }
 }
--- App4/App4/ViewModels/Page2ViewModel.cs
+++ App4/App4/ViewModels/Page2ViewModel.cs
@@ -2,6 +2,7 @@
 using App4.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,28 @@ namespace App4.ViewModels
 {
     class Page2ViewModel : ViewModelBase
     {
+        //UTILISATEUR UTILISER UNIQUEMENT ICI
+        public class UtilisateurVM : ViewModelBase
+        {
+            private String _NomUtilisateur;
+            public String NomUtilisateur
+            {
+                get { return _NomUtilisateur; }
+                set { RaisePropertyChanged(ref _NomUtilisateur, value); }
+            }
+
+            private Int32 _Age;
+            public Int32 Age
+            {
+                get { return _Age; }
+                set { RaisePropertyChanged(ref _Age, value); }
+            }
+
+        }
+
+        //PROPRIETE
+        public Command RefreshCommand { get; set; }
+
         private String _Version;
         public String Version
         {
@@ -26,22 +49,67 @@ namespace App4.ViewModels
             set { RaisePropertyChanged(ref _Model, value); }
         }
 
-        private List<Utilisateur> _ListeUsers = UtilisateurManager.GetFakeUtilisateurs();
-        public List<Utilisateur> ListeUsers
+        private Boolean _IsRefreshing;
+        public Boolean IsRefreshing
+        {
+            get { return _IsRefreshing; }
+            set { RaisePropertyChanged(ref _IsRefreshing, value); }
+        }
+
+        ////Version N°1 de l'observable collection (avec utilisateur)
+        //private ObservableCollection<Utilisateur> _ListeUsers = new ObservableCollection<Utilisateur>(UtilisateurManager.GetFakeUtilisateurs());
+        //public ObservableCollection<Utilisateur> ListeUsers
+        //{
+        //    get { return _ListeUsers; }
+        //    set { RaisePropertyChanged(ref _ListeUsers, value); }
+        //}
+
+        
+        //Version N°2 de l'observable collection (avec utilisateurVM, la nested class)
+        private ObservableCollection<UtilisateurVM> _ListeUsers = new ObservableCollection<UtilisateurVM>();
+        public ObservableCollection<UtilisateurVM> ListeUsers
         {
             get { return _ListeUsers; }
             set { RaisePropertyChanged(ref _ListeUsers, value); }
         }
 
 
-
-        //Instantiacion de l'ioc (connecteur entre l'app et le système)
+        //Instantiacion de l'ioc (Inversion of Control)  (role de connecteur entre l'app et le système)
         IPlatformInfo platInfo = DependencyService.Get<IPlatformInfo>();
 
+
+        //CONSTRUCTEUR
         public Page2ViewModel()
         {
+            //Recuperation des infos via l'ioc (Inversion of Control)
             Model = platInfo?.GetModel();
             Version = platInfo?.GetVersion();
+
+            InitCommand();
+
+            //Boucle pour passer des Utilisateurs aux UtilisateursVM
+            foreach (Utilisateur item in UtilisateurManager.GetFakeUtilisateurs())
+            {
+                ListeUsers.Add(new UtilisateurVM() { NomUtilisateur = item.NomUtilisateur, Age = DateTime.Now.Year - item.DateDeNaissance.Year });
+            }
+        }
+
+        private void InitCommand()
+        {
+            //Commande lors du refresh (scroll vers le haut quand deja top)
+            RefreshCommand = new Command(() =>
+            {
+                ////Ajouter un element au refresh
+                //ListeUsers.Insert(0, new Utilisateur { NomUtilisateur = "OBI-1" });
+
+                ////Supprimer un element
+                //ListeUsers.RemoveAt(0);
+
+                ////Modifier un element
+                //ListeUsers[0].NomUtilisateur = "Aragorn";
+
+                IsRefreshing = false;
+            });
         }
 
     }
